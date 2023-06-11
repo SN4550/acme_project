13b5fea656e053699d8aaccda769cf061c6ad4da
SN4550 <antique.crow@yandex.ru>
2023-06-12 00:02:29 +0300

    form 2

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,26 +1,10 @@
 from django import forms
 
+from .models import Birthday
 
-class BirthdayForm(forms.Form):
-    first_name = forms.CharField(
-        label='Имя', max_length=20)
-    last_name = forms.CharField(
-        label='Фамилия', help_text='Необязательное поле', required=False)
-    birthday = forms.DateField(
-        label='Дата рождения',
-        widget=forms.DateInput(attrs={'type': 'date'}))
-    # title = forms.CharField(
-    #     label='Название:', max_length=20, required=True)
-    # description = forms.CharField(
-    #     label='Описание:',
-    #     required=True,
-    #     widget=forms.Textarea(attrs={'type': 'text'}))
-    # price = forms.IntegerField(
-    #     label='Цена:',
-    #     min_value=10,
-    #     max_value=100,
-    #     help_text='Рекомендованная розничная цена')
-    # comment = forms.CharField(
-    #     label='Комментарий:',
-    #     required=False,
-    #     widget=forms.Textarea(attrs={'type': 'text'}))
+
+class BirthdayForm(forms.ModelForm):
+    class Meta:
+        model = Birthday
+        fields = ['first_name', 'last_name', 'birthday']
+        widget = {'birthday': forms.DateInput(attrs={'type': 'date'})}
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,14 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpRequest, HttpResponse
 
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
+from .models import Birthday
 
 
-def birthday(request):
-    form = BirthdayForm(request.GET or None)
-    context = {'form': form}
+def birthday(request: HttpRequest, pk: int = None) -> HttpResponse:
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
+    form = BirthdayForm(request.POST or None,
+                        instance=instance)
+    context: dict = {'form': form}
     if form.is_valid():
+        form.save()
         birthday_countdown = calculate_birthday_countdown(
             form.cleaned_data['birthday'])
         context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context=context)
+    return render(request=request,
+                  template_name='birthday/birthday.html',
+                  context=context)
+
+
+def birthday_list(request: HttpRequest) -> HttpResponse:
+    birthdays = Birthday.objects.all()
+    context: dict = {'birthdays': birthdays}
+    return render(request=request,
+                  template_name='birthday/birthday_list.html',
+                  context=context)
+
+
+def delete_birthday(request: HttpRequest, pk: int) -> HttpResponse:
+    instance = get_object_or_404(Birthday, pk=pk)
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('birthday:list')
+    return render(request=request,
+                  template_name='birthday/birthday.html',
+                  context=context)
